f41d446408b9213dbba60636196b81b021ad72ff
levanthuan2001qttl <81482705+levanthuan2001qttl@users.noreply.github.com>
2022-06-01 11:29:10 +0700

    ui

--- index.js
+++ index.js
@@ -1,8 +1,114 @@
+"use strict";
+//#region 0. Khai báo biến
+//#region Element node
 const addNewTaskButton = document.querySelector("#add-new-task-btn");
 const submitButton = document.querySelector("#submit-btn");
 const taskListElement = document.querySelector("#task-list");
 //#endregion
 
+submitButton.onclick = () => {
+  // Reset giá trị cho tất cả các biến tính toán về bằng 0, bằng ""
+  resetValueOfAllVariable();
+  //#region 4 element nodelist
+  scheduledProgressNodelist = document.querySelectorAll(
+    ".scheduled-progress-input"
+  );
+  actualProgressNodelist = document.querySelectorAll(".actual-progress-input");
+  budgetNodelist = document.querySelectorAll(".budget-input");
+  costNodelist = document.querySelectorAll(".cost-input");
+  //#endregion
+  //#region 4 values array
+  scheduledProgressValues = Array.from(scheduledProgressNodelist).map(
+    (element) => Number(element.value)
+  );
+  actualProgressValues = Array.from(actualProgressNodelist).map((element) =>
+    Number(element.value)
+  );
+  budgetValues = Array.from(budgetNodelist).map((element) =>
+    Number(element.value)
+  );
+  costValues = Array.from(costNodelist).map((element) => Number(element.value));
+  //#endregion
+  //#region Tính PV, AC, EV
+  const taskNumber = scheduledProgressValues.length;
+  // Validation ...
+  if (
+    handleInvalidFiled(scheduledProgressValues) &&
+    handleInvalidFiled(actualProgressValues) &&
+    handleInvalidFiled(budgetValues) &&
+    handleInvalidFiled(costValues)
+  )
+    if (
+      handlePercentFiled(scheduledProgressValues) &&
+      handlePercentFiled(actualProgressValues)
+    ) {
+      for (let i = 0; i < taskNumber; i++) {
+        let PV = parseFloat(
+          (scheduledProgressValues[i] * parseFloat(budgetValues[i])) / 100
+        );
+        let AC = parseFloat(costValues[i]);
+        let EV = parseFloat(
+          (actualProgressValues[i] * parseFloat(budgetValues[i])) / 100
+        );
+        PVs.push(PV);
+        ACs.push(AC);
+        EVs.push(EV);
+      }
+      PVValue = Number(
+        PVs.reduce((total, currentValue) => total + currentValue).toFixed(0)
+      );
+      ACValue = Number(
+        ACs.reduce((total, currentValue) => total + currentValue).toFixed(0)
+      );
+      EVValue = Number(
+        EVs.reduce((total, currentValue) => total + currentValue).toFixed(0)
+      );
+      //#endregion
+      //#region Tính CPI, SPI, CPIConclusion, SPIConclusion, Cost Variances, Schedule Variance
+      CPI = Number((EVValue / ACValue).toFixed(3));
+      SPI = Number((EVValue / PVValue).toFixed(3));
+      CPIConclusion =
+        CPI > 1
+          ? "Under Budget"
+          : CPI == 1
+          ? "Exactly on budget"
+          : "Over budget";
+      SPIConclusion =
+        CPI > 1
+          ? "Ahead of schedule"
+          : CPI == 1
+          ? "Exactly on schedule"
+          : "Behind schedule";
+      costVariance = Number((((EVValue - ACValue) * 100) / EVValue).toFixed(2));
+      scheduleVariance = Number(
+        (((EVValue - PVValue) * 100) / PVValue).toFixed(2)
+      );
+      //#endregion
+      //#region Tính TotalBudget, ETC, EAC, ProjectedBudgetOverrun
+      totalBudget = Number(
+        budgetValues
+          .reduce((total, currentValue) => total + currentValue)
+          .toFixed(0)
+      );
+      ETC = Number(((totalBudget - EVValue) / CPI).toFixed(0));
+      EAC = Number((ACValue + ETC).toFixed(0));
+      projectedBudgetOverrun = Number((EAC - totalBudget).toFixed(0));
+      //#endregion
+      // Hiển thị các giá trị tính toán được ra màn hình theo đúng vị trí
+      showCalculationResults();
+    } else {
+      toastr.error(
+        "Scheduled progress (Task #1) must be between 0% and 100%.",
+        { timeOut: 1000, progressBar: true }
+      );
+    }
+  else {
+    toastr.error("Please enter all filed", {
+      progressBar: true,
+      timeOut: 1000,
+    });
+  }
+};
 //#region Các biến chứa giá trị tính toán
 let PVs = [],
   ACs = [],
@@ -73,18 +179,18 @@ function resetValueOfAllVariable() {
 
 function showCalculationResults() {
   totalBudgetResultElement.innerHTML = ` $${totalBudget}`;
-  ACResultElement.innerHTML =  `$${ACValue}`;
-  EVResultElement.innerHTML =  `$${EVValue}`;
-  PVResultElement.innerHTML =  `$${PVValue}`;
-  CPIResultElement.innerHTML =  `${CPI}`;
-  costVarianceResultElement.innerHTML =  `${costVariance}%`;
-  CPIConclusionResultElement.innerHTML =  `${CPIConclusion}`;
-  SPIResultElement.innerHTML =  `${SPI}`;
-  scheduleVarianceResultElement.innerHTML =  `${scheduleVariance}%`;
-  SPIConclusionResultElement.innerHTML =  `${SPIConclusion}`;
-  ETCResultElement.innerHTML =  `$${ETC}`;
-  EACResultElement.innerHTML = `$${EAC}`;
-  projectedBudgetOverrunResultElement.innerHTML =  `$${projectedBudgetOverrun} over budget`;
+  ACResultElement.innerHTML = ` $${ACValue}`;
+  EVResultElement.innerHTML = ` $${EVValue}`;
+  PVResultElement.innerHTML = ` $${PVValue}`;
+  CPIResultElement.innerHTML = ` ${CPI}`;
+  costVarianceResultElement.innerHTML = ` ${costVariance}%`;
+  CPIConclusionResultElement.innerHTML = ` ${CPIConclusion}`;
+  SPIResultElement.innerHTML = ` ${SPI}`;
+  scheduleVarianceResultElement.innerHTML = ` ${scheduleVariance}%`;
+  SPIConclusionResultElement.innerHTML = ` ${SPIConclusion}`;
+  ETCResultElement.innerHTML = ` $${ETC}`;
+  EACResultElement.innerHTML = ` $${EAC}`;
+  projectedBudgetOverrunResultElement.innerHTML = ` $${projectedBudgetOverrun} over budget`;
 }
 //#endregion
 //#endregion
@@ -154,118 +260,4 @@ toastr.options = {
   hideMethod: "fadeOut",
   extendedTimeOut: "1000",
   positionClass: "toast-top-center",
-};
-//#region 3. LNSK click button "SUBMIT"
-submitButton.onclick = function (event) {
-  // Reset giá trị cho tất cả các biến tính toán về bằng 0, bằng ""
-  resetValueOfAllVariable();
-
-  //#region 4 element nodelist
-  scheduledProgressNodelist = document.querySelectorAll(
-    ".scheduled-progress-input"
-  );
-  actualProgressNodelist = document.querySelectorAll(".actual-progress-input");
-  budgetNodelist = document.querySelectorAll(".budget-input");
-  costNodelist = document.querySelectorAll(".cost-input");
-  //#endregion
-
-  //#region 4 values array
-  scheduledProgressValues = Array.from(scheduledProgressNodelist).map(
-    (element) => Number(element.value)
-  );
-  actualProgressValues = Array.from(actualProgressNodelist).map((element) =>
-    Number(element.value)
-  );
-  budgetValues = Array.from(budgetNodelist).map((element) =>
-    Number(element.value)
-  );
-  costValues = Array.from(costNodelist).map((element) => Number(element.value));
-  //#endregion
-
-  //#region Tính PV, AC, EV
-  const taskNumber = scheduledProgressValues.length;
-  // Validation ...
-  if (
-    handleInvalidFiled(scheduledProgressValues) &&
-    handleInvalidFiled(actualProgressValues) &&
-    handleInvalidFiled(budgetValues) &&
-    handleInvalidFiled(costValues)
-  )
-    if (
-      handlePercentFiled(scheduledProgressValues) &&
-      handlePercentFiled(actualProgressValues)
-    ) {
-      for (let i = 0; i < taskNumber; i++) {
-        let PV = parseFloat(
-          (scheduledProgressValues[i] * parseFloat(budgetValues[i])) / 100
-        );
-        let AC = parseFloat(costValues[i]);
-        let EV = parseFloat(
-          (actualProgressValues[i] * parseFloat(budgetValues[i])) / 100
-        );
-
-        PVs.push(PV);
-        ACs.push(AC);
-        EVs.push(EV);
-      }
-
-      PVValue = Number(
-        PVs.reduce((total, currentValue) => total + currentValue).toFixed(0)
-      );
-      ACValue = Number(
-        ACs.reduce((total, currentValue) => total + currentValue).toFixed(0)
-      );
-      EVValue = Number(
-        EVs.reduce((total, currentValue) => total + currentValue).toFixed(0)
-      );
-      //#endregion
-
-      //#region Tính CPI, SPI, CPIConclusion, SPIConclusion, Cost Variances, Schedule Variance
-      CPI = Number((EVValue / ACValue).toFixed(3));
-      SPI = Number((EVValue / PVValue).toFixed(3));
-
-      CPIConclusion =
-        CPI > 1
-          ? "Under Budget"
-          : CPI == 1
-          ? "Exactly on budget"
-          : "Over budget";
-      SPIConclusion =
-        CPI > 1
-          ? "Ahead of schedule"
-          : CPI == 1
-          ? "Exactly on schedule"
-          : "Behind schedule";
-
-      costVariance = Number((((EVValue - ACValue) * 100) / EVValue).toFixed(2));
-      scheduleVariance = Number(
-        (((EVValue - PVValue) * 100) / PVValue).toFixed(2)
-      );
-      //#endregion
-
-      //#region Tính TotalBudget, ETC, EAC, ProjectedBudgetOverrun
-      totalBudget = Number(
-        budgetValues
-          .reduce((total, currentValue) => total + currentValue)
-          .toFixed(0)
-      );
-      ETC = Number(((totalBudget - EVValue) / CPI).toFixed(0));
-      EAC = Number((ACValue + ETC).toFixed(0));
-      projectedBudgetOverrun = Number((EAC - totalBudget).toFixed(0));
-      //#endregion
-
-      // Hiển thị các giá trị tính toán được ra màn hình theo đúng vị trí
-      showCalculationResults();
-    } else {
-      toastr.error(
-        "Scheduled progress (Task #1) must be between 0% and 100%.",
-        { timeOut: 1000, progressBar: true }
-      );
-    }
-  else {
-    toastr.error("Please enter all filed", {
-      progressBar: true,
-      timeOut: 1000,
-    });
-  }
 };
